4d0aebdbb91404ef23faf9d9b9845944fce90fa0
Noah House <noahhouse417@gmail.com>
2025-08-22 13:58:43 -0700

    created more flexibility to include trained models, and potential for changing annotations.yaml in training

--- Tracking_F1_Racing_Lines/f1_racing_lines.py
+++ Tracking_F1_Racing_Lines/f1_racing_lines.py
@@ -7,23 +7,34 @@ replicate this project.
 """
 
 # Standard Library imports
-
+from pathlib import Path
 
 # Third Party imports
 from ultralytics import YOLO, settings
 
 
-class TrackCars():
+class TrackCars:
     """
     This class will train the model and perform inference.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, model_path: str | Path | None = None, yaml_path: str | Path | None = None) -> None:
         """
-        Initialize the model.
+        Initialize the model. Both model and yaml path have to be None or have values.
+
+        Inputs:
+            - model: str = path to the model that you want to train
         """
 
-        self.model = YOLO(model='yolo12s.pt')
+        # if given a model then assign it along with the yaml path
+        if model_path is not None and yaml_path is not None:
+            self.model_path = YOLO(model=model_path)
+            self.yaml_path = yaml_path
+        else:
+            self.model_path = YOLO(model='yolo12s.pt')
+            self.yaml_path = "annotations/data.yaml"
+
+        # variable for saving results
         self.results: dict | None
 
         # change ultralytics settings for MLflow
@@ -35,15 +46,13 @@ class TrackCars():
         Split the data into train and test. Then train the model using YOLOv12.
         """
 
-        self.results = self.model.train(data="annotations/data.yaml", epochs=50, imgsz=640, device='mps')
+        self.results = self.model_path.train(data=self.yaml_path, epochs=50, imgsz=640, device='mps')
 
         if self.results is not None:
             return self.results
 
 
-
 if __name__ == "__main__":
 
-    model = TrackCars()
+    model = TrackCars(model_path='runs/detect/train2/weights/best.pt')
     results = model.train_model()
-    
